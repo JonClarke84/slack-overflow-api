d963cec242dee84f7ae1d34e7d2ad54f0478856a
Daniel Taplin <98479830+dantaplin992@users.noreply.github.com>
2022-05-12 16:14:39 +0100

    no modules

--- routes/sessions.js
+++
@@ -1,8 +0,0 @@
-const express = require("express")
-const router = express.Router()
-
-const SessionsController = require("../controllers/sessions")
-
-router.post("/new", SessionsController.New)
-
-module.exports = router
